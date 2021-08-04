74c6e211e3c051e9728a983fa486588662067da6
Your Name <you@example.com>
2021-08-04 23:56:28 +0800

    Update 2021-08-04T15:56:24.813Z

--- app/assets/js/all.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(() => {
-  $("#verified-area").click(function() {
-    $("#verified-dropup-icon").toggleClass( "dropup" )
-  });
-});
---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(document).ready(function () {
+  $("#verified-area").click(function () {
+    $("#verified-dropup-icon").toggleClass("dropup");
+  });
+});
+//# sourceMappingURL=all.js.map
